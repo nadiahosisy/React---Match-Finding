5983f7c60e21376dbc0146df403a58df59b708b1
Your Name <nadiahosisy@gmail.com>
2023-09-25 14:00:34 +0300

    added file data.js

--- Match Finding/src/MainComponent.jsx
+++ Match Finding/src/MainComponent.jsx
@@ -11,62 +11,6 @@ const MainComponent = () => {
   const [afterGame, setAfterGameText] = useState("");
   const [buttonsDisabled, setButtonsDisabled] = useState(false);
 
-  const objects = [
-    {
-      image: "lion.jpg",
-      text: "This is a lion",
-    },
-    {
-      image: "cat.jpg",
-      text: "This is a cat",
-    },
-    {
-      image: "dog.jpg",
-      text: "This is a dog",
-    },
-    {
-      image: "panda.jpg",
-      text: "This is a panda",
-    },
-    {
-      image: "zebra.jpg",
-      text: "This is a zebra",
-    },
-    {
-      image: "fox.jpg",
-      text: "This is a fox",
-    },
-    {
-      image: "pig.jpg",
-      text: "This is a pig",
-    },
-  ];
-
-  const textArray = [
-    "This is a cat",
-    "This is a dog",
-    "This is a lion",
-    "This is a panda",
-    "This is a zebra",
-    "This is a fox",
-    "This is a pig",
-  ];
-  const arrayImages = [
-    "lion.jpg",
-    "cat.jpg",
-    "dog.jpg",
-    "panda.jpg",
-    "zebra.jpg",
-    "fox.jpg",
-    "pig.jpg",
-  ];
-
-  const afterGameText = [
-    "You seem to know animals well, good job",
-    "You look like someone to know few animals, keep trying",
-    "You should watch national geographic chanel more my friend :)",
-  ];
-
   useEffect(() => {
     if (gameCount === 10) {
       const timeoutId = setTimeout(() => {
